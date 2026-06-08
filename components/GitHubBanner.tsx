aa673bb8ea618b672b6fc22ada85b79697165aba
myokooo2004 <myokooo2004@gmail.com>
2026-06-08 19:40:42 +0630

    Update GitHubBanner.tsx

--- components/GitHubBanner.tsx
+++ components/GitHubBanner.tsx
@@ -3,7 +3,7 @@ export default function GitHubBanner() {
   return (
     <div className="flex items-center justify-center rounded-xl border border-gray-200 bg-gray-50 px-4 py-3 shadow-sm">
       <p className="text-xs font-medium text-gray-700">
-        ဖီးနစ် (ထူးကြီး) မှ ပြုပြင်မွန်းမံထားသည်
+        ဖီးနစ် (ထူးကြီး) မှ ပြုပြင်မွမ်းမံထားသည်
       </p>
     </div>
   );
