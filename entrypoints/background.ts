3c7150c9dba0b9aa2eea1030444ebae44bf59a0c
myokooo2004 <myokooo2004@gmail.com>
2026-06-09 11:09:53 +0630

    Update background.ts

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,3 +1,13 @@
 export default defineBackground(() => {
-  console.log('Hello background!', { id: browser.runtime.id });
+  console.log('CEIR Extension background started', { id: browser.runtime.id });
+
+  browser.runtime.onMessage.addListener((message, _sender, sendResponse) => {
+    if (message.type === 'FETCH_TAC_DB') {
+      fetch(message.url)
+        .then(res => res.json())
+        .then(data => sendResponse({ data }))
+        .catch(err => sendResponse({ error: String(err) }));
+      return true;
+    }
+  });
 });
