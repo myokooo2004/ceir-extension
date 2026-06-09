9630746f0eba49c9c98722aad39e70982cce02a6
myokooo2004 <myokooo2004@gmail.com>
2026-06-09 11:09:15 +0630

    Update wxt.config.ts

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,13 +1,15 @@
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'wxt';
 
-// See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ['@wxt-dev/module-react'],
   vite: () => ({
     plugins: [tailwindcss()]
   }),
   manifest: {
-    host_permissions: ['https://ceir.gov.mm/check-status'],
+    host_permissions: [
+      'https://ceir.gov.mm/*',
+      'https://raw.githubusercontent.com/*',
+    ],
   },
 });
